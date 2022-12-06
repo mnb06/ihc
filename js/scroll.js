c43e3b00915f838ca5cb9ffad9c188efe2387a8a
Matias Bue <matias.bue@outlook.com>
2022-12-06 14:30:36 -0800

    eliminados indicadores graficos de la seccion actual y cambio de velocidad en scroll

--- js/scroll.js
+++ js/scroll.js
@@ -3,29 +3,5 @@ $.scrollify({
 	scrollbars:"false",
 	interstitialSection : "#header,#footer",
 	easing: "swing",  
-    scrollSpeed: 300,  
-
-
-	//以下、ページネーション設定
-	before:function(i,panels) {
-		var ref = panels[i].attr("data-section-name");
-		  $(".pagination .active").removeClass("active");
-		  $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
-		},
-		afterRender:function() {
-		  var pagination = "<ul class=\"pagination\">";
-		  var activeClass = "";
-		  $(".box").each(function(i) {//1ページスクロールさせたいエリアクラス名を指定
-			activeClass = "";
-			if(i===$.scrollify.currentIndex()) {
-			  activeClass = "active";
-			}
-			pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
-		  });
-		  pagination += "</ul>";
-	
-		  $("#box1").append(pagination);//はじめのエリアにページネーションを表示
-		  $(".pagination a").on("click",$.scrollify.move);
-		}
-	
-	  });
+    scrollSpeed: 600	
+});
